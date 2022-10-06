6417a51634ad3bb9970ee273dbbc2f2931b8d0fd
Gabbynts <gabriella.natasya2003@gmail.com>
2022-10-06 16:59:27 +0700

    feat : services

--- Personal_Portofolio/assets/js/main.js
+++ Personal_Portofolio/assets/js/main.js
@@ -68,3 +68,26 @@ tabsContent.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+/*==================================== SERVICES MODAL ======================================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button')
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i)=>{
+    modalBtn.addEventListener('click', ()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose)=>{
+    modalClose.addEventListener('click', ()=>{
+        modalViews.forEach((modalViews, i)=>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
