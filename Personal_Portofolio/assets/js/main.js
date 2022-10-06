fe7da0cab82363309b026e9f71de61a526028dc0
Gabbynts <gabriella.natasya2003@gmail.com>
2022-10-06 14:32:53 +0700

    add : qualification

--- Personal_Portofolio/assets/js/main.js
+++ Personal_Portofolio/assets/js/main.js
@@ -48,3 +48,23 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+/*==================================== QUALIFICATION TABS ======================================*/
+const tabs = document.querySelectorAll('[data-target'),
+    tabsContent = document.querySelectorAll('[data-content]')
+
+tabsContent.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabsContents.forEach(tabsContent =>{
+            tabsContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
